a29888691d74395b68d29fdaa9ec6f5b384dacb9
itskrixhna32 <itskrixhna32@gmail.com>
2026-02-03 14:39:20 +0000

    Check whether a given number is multiple of five or not.

---
+++ 2.2.py
@@ -0,0 +1,6 @@
+#2.	Check whether a given number is multiple of five or not.
+num = int(input("enter the number: "))
+if num%5==0:
+    print("the number is multuiple of 5.")
+else:
+    print("the number is not multiple of 5.")

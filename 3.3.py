264fecac521945103183bb75582d0a5e0f5c404a
itskrixhna32 <itskrixhna32@gmail.com>
2026-02-04 18:34:24 +0000

    Print Fibonacci series up to given term

---
+++ 3.3.py
@@ -0,0 +1,17 @@
+#3.	Print Fibonacci series up to given term
+nterms = int(input("enter the number of terms: "))
+n1,n2 = 0,1
+count = 0
+if nterms<=0:
+    print("please enter a positive integer.")
+elif nterms==1:
+    print("Fibonacci sequence upto",nterms,":")
+    print(n1)
+else:
+    print("Fibonacci sequence: ")
+    while count<nterms:
+        print(n1,end=' ')
+        nth = n1+n2
+        n1 = n2
+        n2 = nth
+        count+=1

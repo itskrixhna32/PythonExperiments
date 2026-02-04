e12c2fe32744df8ff98e259c0f83aa76622c9e26
itskrixhna32 <itskrixhna32@gmail.com>
2026-02-04 18:39:40 +0000

    1234554321 1234 * 4321 123 * * 321 12 * * * 21 1 * * * * 1  printing this pattern

---
+++ 3.10.py
@@ -0,0 +1,13 @@
+n = 5
+
+for i in range(n):
+    for j in range(1, n - i + 1):
+        print(j, end="")
+
+    if i > 0:
+        print(" " + "* " * i, end="")
+
+    for j in range(n - i, 0, -1):
+        print(j, end="")
+
+    print()

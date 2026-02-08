fa988c7872dd2bf579c8cb65714a4102c7f6e280
itskrixhna32 <itskrixhna32@gmail.com>
2026-02-08 18:25:17 +0000

    solving the equation 4*x*x*x*x+3*y*y*y+9*z+6*n

---
+++ 1.3.py
@@ -0,0 +1,6 @@
+n = int(input("enter any number: "))
+x = int(input("enter the value of x: "))
+y = int(input("enter the value of y: "))
+z = int(input("enter the value of z: "))
+equation = 4*x*x*x*x+3*y*y*y+9*z+6*n
+print(equation)

12da251af4231103253c3f410da4ba7eed8a4f99
itskrixhna32 <itskrixhna32@gmail.com>
2026-02-08 18:24:18 +0000

    number is fully divisible or not

---
+++ 1.2.py
@@ -0,0 +1,6 @@
+# number is fully divisible or not
+n = int(input("enter any number: "))
+if(n%2 == 0 and n%3 == 0):
+  print("number is fully divisible")
+else:
+  print("number is not fully divisible.")
